0faf163aa1edf22212352263ce6729bf32d31634
SunnySparks <Josediazt6@outlook.com>
2021-03-17 21:22:34 -0600

    Create events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_221109) do
+ActiveRecord::Schema.define(version: 2021_03_18_030755) do
+
+  create_table "events", force: :cascade do |t|
+    t.date "date"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
