22ae260dbc0d2e34703ed2b971b316ea1c6c69a0
SunnySparks <Josediazt6@outlook.com>
2021-03-18 14:47:38 -0600

    basic events creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,15 +21,7 @@ class EventsController < ApplicationController
     end
   end
 
-  def rsvp
-    @event = Event.find(params[:id])
-    if @event.attendees.include?(current_user)
-      redirect_to @event, notice: "You are already on the list"
-    else
-      @event.attendees << current_user
-      redirect_to @event
-    end
-  end
+
 
 
   private
