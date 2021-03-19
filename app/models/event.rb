c14eb898383835438e7d192874a306eb67aa454c
SunnySparks <Josediazt6@outlook.com>
2021-03-18 20:10:44 -0600

    create relations between user and events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
     belongs_to :creator, foreign_key: :creator_id, class_name: "User"
+    
+    has_many :attendees, foreign_key: :attendee_id, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_many :created_events, foreign_key: :created_event_id, class_name: "Event"
+
+    has_many :attended_events, foreign_key: :attendance_event_id, class_name: "Event"
 end
