515c22f57a6ebc5802af4092bc0047d3b54119fa
SunnySparks <Josediazt6@outlook.com>
2021-04-03 18:28:28 -0600

    Finish project's minimum requirements

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,5 +5,5 @@ class Event < ApplicationRecord
   belongs_to :creator, foreign_key: :creator_id, class_name: 'User'
 
   scope :past, -> { where('date < ?', Date.today) }
-  scope :upcoming, -> { where('start >= ?', Date.today) }
+  scope :upcoming, -> { where('date >= ?', Date.today) }
 end
